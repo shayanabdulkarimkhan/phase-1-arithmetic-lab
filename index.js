d64f1707b25abc933f1cef21261b592117801035
shayan <sak704@nyu.edu>
2022-02-06 15:28:30 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 15.5;
+const num2 = 4;
+const num3 = 24;
+const num4 = 20;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) +1;
+
+let mod = num3 % num4;
+
+let max = Math.max(2,3,4,20);
+
